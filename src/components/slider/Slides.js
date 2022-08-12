154d30a37e48b31230b807d6a90955eca100a59e
DrearyStudent <Jeff_With@hotmail.com>
2022-08-12 11:45:40 -0500

    added functionality

--- src/components/slider/Slides.js
+++ src/components/slider/Slides.js
@@ -6,15 +6,17 @@ const slidesInfo = [
     {
         src:"https://cdn.pixabay.com/photo/2016/11/30/20/44/computer-1873831_1280.png",
         alt: "Slider 1",
-        desc: "GitHub"
+        desc: "GitHub",
+        href: "https://github.com/DrearyStudent"
 
     },
 
     {
         
-        src:"https://cdn.pixabay.com/photo/2015/07/17/22/43/student-849825_1280.jpg",
+        src:"https://cdn.pixabay.com/photo/2015/07/17/22/43/student-849825_1280.jpg ",
         alt: "Slider 2",
-        desc: "Colaboración en proyectos"
+        desc: "Colaboración en proyectos",
+        href: "https://solo.to/drearystudent"
 
     },
 
@@ -22,7 +24,8 @@ const slidesInfo = [
         
         src:"https://cdn.pixabay.com/photo/2017/12/31/12/38/adult-3052244_1280.jpg",
         alt: "Slider 3",
-        desc: "Página web de documentación de proyecto y diseño de software"
+        desc: "Página web de documentación de proyecto y diseño de software",
+        href: "https://phalord.github.io/PracticasProfesionales/"
 
     }
 
@@ -32,7 +35,10 @@ const slides =slidesInfo.map( slide => (
 
     <div className="slide-container">
 
-        <img src={slide.src} alt={slide.alt} ></img>
+        <a href={slide.href} target="_blank">
+            <img src={slide.src} alt={slide.alt} ></img>
+        </a>
+        
 
         <div className="slide-description">
 
